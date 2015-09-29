adbb9757753c4f7dcbc478d29e71afdf5a68a588
Elana Blom <elana.blom@s4integration.co.za>
2015-09-29 11:20:49 +0200

    Updated Navigation Menu Items

--- app/src/main/java/za/co/timesheetapp/MainActivity.java
+++ app/src/main/java/za/co/timesheetapp/MainActivity.java
@@ -58,13 +58,13 @@ public class MainActivity extends AppCompatActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = "Manage Leave";
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = "Contacts";
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = "TimeSheet";
                 break;
         }
     }
